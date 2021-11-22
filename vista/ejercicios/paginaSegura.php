17525210c1b0ecef735a8d73704346857e7ab0cb
mistchenco <augusto.perceval@est.fi.uncoma.edu.ar>
2021-11-22 16:38:18 -0300

    pag segura

--- vista/ejercicios/paginaSegura.php
+++ vista/ejercicios/paginaSegura.php
@@ -13,15 +13,10 @@ if (!$sesion->activa()) {
 }
 echo "</br></br></br></br></br></br>";
 echo "<h4>Usted esta Logueado como: {$objUsuario->getUsNombre()} con el Rol: {$sesion->getRolActivo()->getRolDescripcion()}</h4>";
-$abmUsuario = new abmUsuario();
-$abmRol = new abmUsuarioRol();
-$listaUsuario = $abmUsuario->buscar(null);
-if ($tienePermiso == false) {
-  echo "</br></br></br></br></br></br>";
-  echo "<h4 class='alert alert-danger'>Usted no tiene Permisos para esta seccion</h4>";
-} else {
+
+
 ?>
 <?php
-}
+
 include_once '../estructura/footer.php';
 ?>
